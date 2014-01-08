3c79bf6aacdf5a684b335e6d80bb030eb4ef9125
Riaku <riaku56@gmail.com>
2014-01-07 21:38:49 -0600

    Going to bed, will figure out other stuff tomorrow
    Added Blueprint addition on pickup

--- src/main/java/net/riaku/blueprintListener.java
+++ src/main/java/net/riaku/blueprintListener.java
@@ -2,14 +2,18 @@ package net.riaku;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,26 +41,24 @@ public class blueprintListener implements Listener
             player.sendRawMessage("You have not discovered this blueprint.");
         }
     }
-
     @EventHandler
-    public void onInteract(PlayerInteractEvent event)
-    {
+    public void onPickup(PlayerPickupItemEvent event){
         Player p = event.getPlayer();
-        ItemStack IS = p.getItemInHand();
+        ItemStack IS = event.getItem().getItemStack();
         ItemMeta IM = IS.getItemMeta();
         if (IS.getType() == Material.PAPER && IM.getDisplayName().equals("Blueprint"))
         {
             List<String> lore = IM.getLore();
-            FileConfiguration BPC = BP.getConfig();
-            List<Integer> playerCon = BPC.getIntegerList("players." + p.getName());
+            FileConfiguration blueprintConfig = BP.getConfig();
+            List<Integer> playerCon = blueprintConfig.getIntegerList("players." + p.getName());
             if(!playerCon.contains(Integer.parseInt(lore.get(0)))){
                 playerCon.add(Integer.parseInt(lore.get(0)));
-                BPC.set("players." + p.getName(), playerCon);
-                System.out.println(BPC.getStringList("players." + p.getName()));
+                blueprintConfig.set("players." + p.getName(), playerCon);
                 p.sendRawMessage("Adding recipe for: " + lore.get(0) + " - " + lore.get(1));
-                event.getItem().setAmount(event.getItem().getAmount() -1);
-            } else {
-                p.sendRawMessage("You have already discovered this blueprint.");
+            }
+            if(!BP.getConfig().getBoolean("keepOnPickup")){
+                ItemStack AIRstack = new ItemStack(Material.AIR);
+                event.getItem().setItemStack(AIRstack);
             }
         }
     }
@@ -65,18 +67,19 @@ public class blueprintListener implements Listener
     public void onDeath(EntityDeathEvent event)
     {
         int chance = BP.getConfig().getInt("dropChance");
-        Integer rand = (int) (Math.random() * 100);
+        Integer rand = (int)(Math.random() * 100);
         if (rand <= chance)
         {
             ItemStack IS = new ItemStack(Material.PAPER);
             ItemMeta IM = IS.getItemMeta();
             IM.setDisplayName("Blueprint");
-            List<String> ABP = BP.getConfig().getStringList("Blueprints");
+            List<String> ABP = BP.getConfig().getStringList("blueprints");
             Integer randItemID = Integer.parseInt(ABP.get((int) (Math.random() * ABP.size())));
             List<String> lore = Arrays.asList(randItemID.toString(), IDAPI.getMaterialById(randItemID).name());
             IM.setLore(lore);
             IS.setItemMeta(IM);
             event.getDrops().add(IS);
+
         }
 
     }
@@ -84,7 +87,6 @@ public class blueprintListener implements Listener
     private Boolean hasBlueprint(Material mat, Player p)
     {
         List<String> pidlist = BP.getConfig().getStringList("players." + p.getName());
-
         return pidlist.contains(String.valueOf(IDAPI.getIdOfMaterial(mat)));
     }
 }
